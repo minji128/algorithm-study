b6454163d463e8bd375faf6e5f2b722e0787a41f
minji128 <minji.lee128@gmail.com>
2021-10-29 23:32:52 +0900

    Add: 전화번호 목록 문제

---
+++ programmers/Level2/python/전화번호 목록.py
@@ -0,0 +1,10 @@
+from itertools import combinations
+
+def solution(phone_book):
+    answer = True
+    phone_book.sort()
+    for i, j in zip(phone_book, phone_book[1:]):
+        if j.startswith(i):
+            answer = False
+            break
+    return answer
